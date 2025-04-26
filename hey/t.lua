a4f8bb4b48f7c6ad294a17c53c068b7391ca5239
ccrrrr1 <ccrrrr100@gmail.com>
2025-04-26 19:54:33 -0400

    Update t.lua

--- hey/t.lua
+++ hey/t.lua
@@ -1,2 +1,6 @@
 warn("LOLL!!")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ccrrrr1/CCR.CC-Hub-New/refs/heads/main/hey/no.lua"))()
+_G.nooooo = true
+_G.VV = "2.0"
+_G.hi = "HI"
+_G.shutdown = false
